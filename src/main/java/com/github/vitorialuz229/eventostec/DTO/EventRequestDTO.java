3ab28db6cfa9ac1351a2871c17669c145af850b9
vitorialuz229 <vitorialuz831@gmail.com>
2024-06-25 19:15:47 -0300

    feat: creating DTO

--- src/main/java/com/github/vitorialuz229/eventostec/DTO/EventRequestDTO.java
+++ src/main/java/com/github/vitorialuz229/eventostec/DTO/EventRequestDTO.java
@@ -2,5 +2,5 @@ package com.github.vitorialuz229.eventostec.DTO;
 
 import org.springframework.web.multipart.MultipartFile;
 
-public record EventRequestDTO(String title, String description, Long date, String city, String uf, Boolean remote, String eventUrl, MultipartFile imagem) {
+public record EventRequestDTO(String title, String description, Long date, String city, String uf, Boolean remote, String eventUrl, MultipartFile image) {
 }
